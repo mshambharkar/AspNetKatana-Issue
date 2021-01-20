aeac0670085486b4534432cef0a7b56a67616676
Mahindra Shambharkar <mbshambhakar@gmail.com>
2021-01-20 19:36:33 +0530

    Added server

---
+++ Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Swashbuckle.Application;
+using Microsoft.Owin;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        string baseAddress = $"http://localhost:{(args.Length > 0 ? args[0] : "9001")}/";
+
+        // Start OWIN host 
+        using (WebApp.Start<Startup>(url: baseAddress))
+        {
+            Console.WriteLine($"Application Started at: {baseAddress}");
+            Console.WriteLine($"Swagger available at: {baseAddress}swagger/");
+            Console.ReadLine();
+        }
+    }
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.MapHttpAttributeRoutes();
+            config.EnableSystemDiagnosticsTracing();
+            config.EnableSwagger(c =>
+            {
+                c.SingleApiVersion("v1", "OWIN version: " + Assembly.GetAssembly(typeof(OwinContext)).GetName().Version);
+                c.IncludeXmlComments($"{System.AppDomain.CurrentDomain.BaseDirectory}{Assembly.GetExecutingAssembly().GetName().Name}.xml");
+
+            }).EnableSwaggerUi();
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
---
+++ Server/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Http;
+using System.Net;
+
+[RoutePrefix("api/values")]
+public class ValuesController : ApiController
+{
+    [HttpGet]
+    [Route("{Path1}/{Path2}")]
+    public object Get([FromUri]string Path1, [FromUri] string Path2, string query1, string query2, string query3)
+    {
+        return new ResponseModel(Path1, Path2, query1, query2, query3);
+    }
+}
+
+
+public class ResponseModel
+{
+    public string Path1 { get; set; }
+    public string DecodedPath1 { get; set; }
+    public string Path2 { get; set; }
+    public string DecodedPath2 { get; set; }
+    public string Query1 { get; set; }
+    public string DecodedQuery1 { get; set; }
+    public string Query2 { get; set; }
+    public string DecodedQuery2 { get; set; }
+    public string Query3 { get; set; }
+    public string DecodedQuery3 { get; set; }
+    public ResponseModel(string Path1, string Path2, string Query1, string Query2, string Query3)
+    {
+        this.Path1 = Path1;
+        this.Path2 = Path2;
+        this.Query1 = Query1;
+        this.Query2 = Query2;
+        this.Query3 = Query3;
+        this.DecodedPath1 = WebUtility.UrlDecode(Path1);
+        this.DecodedPath2 = WebUtility.UrlDecode(Path2);
+        this.DecodedQuery1 = WebUtility.UrlDecode(Query1);
+        this.DecodedQuery2 = WebUtility.UrlDecode(Query2);
+        this.DecodedQuery3 = WebUtility.UrlDecode(Query3);
+    }
+
+    public override string ToString()
+    {
+        return $"Path1 = {Path1}, Path2 = {Path2}, Query1 = {Query1}, Query2 = {Query2}, Query3 = {Query3}, DecodedPath1 = {DecodedPath1}, DecodedPath2 = {DecodedPath2}, DecodedQuery1 = {DecodedQuery1}, DecodedQuery2 = {DecodedQuery2}, DecodedQuery3 = {DecodedQuery3}";
+    }
+
+}
